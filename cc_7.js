3ca5cf4c6a5d1c8b5c2fb30bd22213643ef92c9d
Sanjay-Sub1 <sanjaysubramanian@usf.edu>
2024-10-06 17:38:53 -0400

    Initialize company structure with departments and employees

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,57 @@
+//Task 1 creates an object company which has departments, employees, and their salaries, as well as a hierarchy through subordinate arrays
 
+const company = {
+    departments: [
+        {
+            departmentName: 'Marketing',
+            employees: [
+                {
+                    name: 'Sanjay',
+                    salary: 100000,
+                    subordinates: [
+                        {
+                            name: 'Jorge',
+                            salary: 80000,
+                            subordinates: [
+                                {
+                                    name: 'Charles',
+                                    salary: 60000,
+                                    subordinates: []
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    name: 'Leila',
+                    salary: 90000,
+                    subordinates: []
+                }
+            ]
+        },
+        {
+            departmentName: 'Sales',
+            employees: [
+                {
+                    name: 'Jai',
+                    salary: 85000,
+                    subordinates: [
+                        {
+                            name: 'Karson',
+                            salary: 70000,
+                            subordinates: []
+                        }
+                    ]
+                },
+                {
+                    name: 'Gabe',
+                    salary: 95000,
+                    subordinates: []
+                }
+            ]
+        }
+    ]
+};
+
+//Used to display the array "Comapny"
+console.log(company);
